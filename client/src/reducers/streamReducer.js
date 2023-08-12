d5675293254486f675c79a0c233a489e0e5ee139
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-08-12 18:06:55 +0600

    Create stream reducer

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,26 @@
+import {
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
+
+import _ from "lodash";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
