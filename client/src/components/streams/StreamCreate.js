dafb4161aedc3b3c7e0528ea09e9e8bc2e0835b0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-08-12 14:35:32 +0600

    Init json server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -11,7 +11,7 @@ class StreamCreate extends React.Component {
       );
     }
   }
-
+ 
   renderInput = ({ input, label, meta }) => {
     const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
